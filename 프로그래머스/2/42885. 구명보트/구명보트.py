7e2837da4fcf9f84ef5012b3bb5ea6664545aadc
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-04-29 15:17:07 +0900

    [level 2] Title: 구명보트, Time: 9.40 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,20 +1,23 @@
 def solution(people, limit):
+    answer = 0
+    # 1. 몸무게를 오름차순으로 정렬
+    people.sort() 
     
-    people.sort()
-    st = []
-    total = 0
-    start = 0
-    end = len(people)-1
+    # 양 끝을 가리키는 두 개의 포인터 설정
+    left = 0 
+    right = len(people) - 1 
     
-    while start <= end:
-        if people[start] +people[end] <= limit:
-            start += 1
-            end -= 1
+    # 포인터가 교차하기 전까지(모든 사람을 검사할 때까지) 반복
+    while left <= right:
+        # 가장 가벼운 사람 + 가장 무거운 사람 <= 제한 무게
+        if people[left] + people[right] <= limit:
+            left += 1  # 가벼운 사람 구출 완료 (오른쪽으로 한 칸 이동)
+            right -= 1 # 무거운 사람 구출 완료 (왼쪽으로 한 칸 이동)
+        # 제한 무게 초과 시 무거운 사람만 탑승
         else:
-            end -= 1
-        total += 1
-    return total
-        
-    
-                
+            right -= 1 # 무거운 사람 혼자 구출 완료 (왼쪽으로 한 칸 이동)
             
+        # 짝을 지었든 혼자 탔든, 보트는 1대 출발함
+        answer += 1
+        
+    return answer
