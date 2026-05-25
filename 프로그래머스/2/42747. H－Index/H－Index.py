8133906c4d5599acbb911aa5e44a8aadf03cad83
dori-2i <asj7020@gmail.com>
2026-05-25 22:35:06 +0900

    [level 2] Title: H-Index, Time: 0.19 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,14 +1,17 @@
 def solution(citations):
+    
     citations.sort(reverse=True)
+    H=0
+    i=0
+    while i<len(citations):
+        if citations[i]>H:
+            i+=1
+            H+=1
+        elif citations[i]<=H:
+            return H
+    return H
     
-    answer = 0
-    
-    for i in range(len(citations)):
-        citation = citations[i]
         
-        if citation >= i+1 :
-            answer=i+1
-        else :
-            break
-            
-    return answer
+    
+    
+    
