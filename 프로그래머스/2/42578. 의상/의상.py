68837dfb8317200ce0863ed54acb3fec89c36c68
dori-2i <asj7020@gmail.com>
2026-07-14 20:33:20 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,19 +1,28 @@
 def solution(clothes):
+    
     hash={}
+    result=1
     
     for x,y in clothes:
         hash[y]=hash.get(y,0)+1
         
-    print(hash)
-    answer=1
-    for count in hash.values():
-        answer*=(count+1)
-            
-    return answer-1
+    for value in hash.values():
+        result*=value+1
         
-        
-        
+    return result-1
+    
+    '''
+    조합론(경우의 수)로 생각한다.
+    (N+1)(M+1)로 했을 때, +1은 아무것도 입지않은 경우이다.
+    (N+1)(M+1) = NM + N + M + 1
+
+    - NM: N과 M을 모두 사용하는 경우
+    - N: N만 사용하는 경우
+    - M: M만 사용하는 경우
+    - 1: 모두 사용하지 않는 경우
+    '''
+    
+   
         
        
-        
     
