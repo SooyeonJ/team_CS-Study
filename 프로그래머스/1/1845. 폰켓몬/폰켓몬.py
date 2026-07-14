de39b53e4baa998c9e5e18cfd23361898b712a4b
dori-2i <asj7020@gmail.com>
2026-07-14 18:42:34 +0900

    [level 1] Title: 폰켓몬, Time: 2.24 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,12 @@
 def solution(nums):
+    hash={}
+    nums.sort()
     
-    selectN = len(nums)//2
+    for x in nums:
+        hash[x]=hash.get(x,0)+1
+        
     
-    if selectN>len(set(nums)):
-        return len(set(nums))
-    elif selectN<=len(set(nums)):
-        return selectN
+    if len(hash.keys())>=len(nums)//2:
+        return len(nums)//2
+    else:
+        return len(hash.keys())
