6e5a6ec7a80546812cf2c6264fe7bfe3045bd5e4
SOOYEON JEONG <100952455+SooyeonJ@users.noreply.github.com>
2026-07-15 17:14:39 +0900

    [level 2] Title: 가장 큰 수, Time: 35.04 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,4 +1,5 @@
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x:x*3, reverse=True)    
-    return str(int(''.join(numbers))) # 앞에 0 여러 개 나오면 하나로 줄여야 함
+    result = ''.join(sorted(map(str, numbers), key=lambda x: x*4, reverse=True))
+    if result[0] == '0':
+        return '0'
+    return result
