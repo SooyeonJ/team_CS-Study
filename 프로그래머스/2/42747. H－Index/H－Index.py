2644988028a72ba4699051f38f1cc15076383d91
dori-2i <asj7020@gmail.com>
2026-07-15 23:46:27 +0900

    [level 2] Title: H-Index, Time: 0.14 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,17 +1,17 @@
 def solution(citations):
-    
+    '''
+    H-Index는 내림차순 정렬하여 인용수>=논문수
+    논문수보다 인용수가 작아지면, 인용수가 n번째 이상인
+    논문수가 최소 n개 이상인 논문 수가 몇 개 이상 있다고
+    말할 수 없다.
+    '''
     citations.sort(reverse=True)
-    H=0
-    i=0
-    while i<len(citations):
-        if citations[i]>H:
-            i+=1
-            H+=1
-        elif citations[i]<=H:
-            return H
-    return H
     
-        
+    for x in citations:
+        if x!=0:
+            return max([i+1 for i,x in enumerate(citations) if i+1<=x]) 
+        else:
+            return 0
     
     
     
