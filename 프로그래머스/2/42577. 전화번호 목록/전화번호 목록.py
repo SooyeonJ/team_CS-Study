3daefa7f036d93df8cbef9e5a424a239225e78eb
dori-2i <asj7020@gmail.com>
2026-07-17 20:14:13 +0900

    [level 2] Title: 전화번호 목록, Time: 70.58 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,17 +1,27 @@
 def solution(phone_book):
-    hash_map={}
     
-    for num in phone_book:
-        hash_map[num]=1
-    
-    for number in phone_book:
-        tp=""
-        for cha in number:
-            tp+=cha
+    '''
+    효율성에서 오류가 난 이유
+    전화번호 길이가 1,000,000인데 이를 2중
+    for문해서 파이썬의 연산이 길어진다.
+    sort()를 하게 되면, 접두어 관계인 문자열들은
+    앞뒤로 나란히 위치하기 때문에
+    2중 for문이 아닌 한바퀴만 돌아도 시간 복잡도가 
+    줄어든다.
+    phone_book.sort()
+    for x in phone_book:
+        for y in phone_book:
+            if x!=y:
+                if x==y[0:len(x)]:
+                        return False
             
-            if tp in hash_map and tp!=number:
-                return False
-       
+    return True
+    '''    
+        
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]): 
+            return False
     
-    answer = True
-    return answer
+    return True
+    
