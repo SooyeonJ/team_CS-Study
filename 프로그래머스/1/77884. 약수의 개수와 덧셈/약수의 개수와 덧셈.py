aa05bf0487905644c3c3072748bded57279892f3
dori-2i <asj7020@gmail.com>
2026-07-20 11:11:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.19 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,3 +1,6 @@
+'''
+내가 풀었던 방식
+--------------------------------------
 def anw(a):
     count=0
     result=[]
@@ -18,7 +21,19 @@ def solution(left,right):
         else: sol.append(-result)
     
     return sum(sol)
-        
+'''
+
+'제곱근을 이용한 풀이 방법'
+
+def solution(left,right):
+    answer=0
+    for i in range(left,right+1):
+        '완전 제곱수인지 확인하는 것, 완전 제곱수는 루트를 씌워도 실수가 아닌 정수가 나오기 때문'
+        if int(i**0.5)==i**0.5:
+            answer-=i
+        else:
+            answer+=i
+    return answer
     
 
         
