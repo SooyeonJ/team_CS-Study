5ed859203716b25802d8eb3530f653b87f92c64f
dori-2i <asj7020@gmail.com>
2026-07-21 21:28:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,18 +1,20 @@
 def solution(num):
-    
-    count=0
-    while num>0:
-        if(num%2==0):
-            num=num//2
-            count+=1
-        elif(num%2!=0):
-            if(num==1):
-                return count
-            else:
-                num=num*3+1
-                count+=1
-        if(count>=500):
+    counter=0
+    while num!=1:
+        if counter>=500:
+            num!=1
             return -1
-            break
+        elif num%2==0:
+            num=num//2
+            counter+=1
+        elif num%2!=0:
+            num=num*3+1
+            counter+=1
+    return counter
+        
+
+        
+    
+            
             
             
