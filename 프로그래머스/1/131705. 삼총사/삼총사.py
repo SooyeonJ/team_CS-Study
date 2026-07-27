5e62d4f479203ee9ee3ad2a3a1bdeb719533ce6c
dori-2i <asj7020@gmail.com>
2026-07-27 10:41:30 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,3 +1,4 @@
+'''
 def solution(number):
     
     sorted(number)
@@ -12,5 +13,23 @@ def solution(number):
         return count
     else:
         return 0
+'''
+
+'combination 함수 쓰는 방법'
+'itertools 모듈에 내장된 combinations 함수를 사용'
+from itertools import combinations
+
+def solution(number):
+    
+    count=0
+    for x in combinations(number,3):
+        if sum(x)==0:
+            count+=1
+    
+    if count>0:
+        return count
+    else: return 0
+
+
     
     
