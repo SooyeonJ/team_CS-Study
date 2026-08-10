5eaabf03c0a6f065efa7ffc14bf69f131122e279
dori-2i <asj7020@gmail.com>
2026-08-10 22:59:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 43.40 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,11 +1,19 @@
 def solution(arr):
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    stack=[]
-    for i in arr:
-        if not stack or stack[-1]!=i:
-            stack.append(i)
-            
-    return stack
     
+    sol=[]
+    for i,j in zip(arr,arr[1:]):
+        if i!=j:
+            sol.append(i)
+    
+    if len(sol)==0 and len(arr)!=0:
+        return [arr[0]]
+    elif len(arr)!=0 and sol[-1]!=arr[-1]:
+        sol.append(arr[-1])
         
+    return sol
+            
+    
+
+    
  
