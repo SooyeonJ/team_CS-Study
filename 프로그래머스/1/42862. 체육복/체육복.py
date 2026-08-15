ae1abe25f6f311cc7cb9643677c9915f45e11ef6
dori-2i <asj7020@gmail.com>
2026-08-15 19:10:01 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,26 +1,25 @@
 def solution(n, lost, reserve):
-    total = [1]*(n+2)
+    '체육복을 가져온 학생도 체육복을 도난당 할 수 있다고 가정-> set 이용'
+    real_lost = set(lost)-set(reserve)
+    real_reserve = set(reserve)-set(lost)
     
-    for i in reserve:
-        total[i]+=1
-    for i in lost:
-        total[i]-=1
-    
-    for i in range(1,n+1):
-        if(total[i]==0):
-            if(total[i-1]==2):
-                total[i]+=1
-                total[i-1]-=1
-            elif(total[i+1]==2):
-                total[i]+=1
-                total[i+1]-=1
-
-    answer=0
-    for i in range(1,n+1):
-        if(total[i]>0):
-            answer+=1
+    '여벌 체육복이 있는 학생 앞/뒤 탐색해서 lost가 있는 경우 해당 순서를 remove한다.'
+    for r in sorted(real_reserve):
+        if r-1 in real_lost:
+            real_lost.remove(r-1)
+        elif r+1 in real_lost:
+            real_lost.remove(r+1)
             
-    return answer
+    return n-len(real_lost)
+    
+    
+               
+            
+            
+                
+    
+    
+            
             
             
             
