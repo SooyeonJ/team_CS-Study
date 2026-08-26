8ef6bdce5d99deec425a0c97c8ae522c9ac21333
dori-2i <asj7020@gmail.com>
2026-08-26 09:57:49 +0900

    [level 1] Title: 콜라 문제, Time: 0.23 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,11 +1,3 @@
 def solution(a, b, n):
-    
-    total=n
-    result=0
-    while total>=a:
-        result+=b*(total//a)
-        total=(total%a)+(b*(total//a))
-    
-    return result
-        
-    
+    answer = 0
+    return answer
