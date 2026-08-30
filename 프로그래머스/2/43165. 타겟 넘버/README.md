632358d289344e8c3a1f3f237eb02198f28bda89
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2026-08-31 07:01:03 +0900

    [level 2] Title: 타겟 넘버, Time: 114.52 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 379.38 ms
+메모리: 11.5 MB, 시간: 114.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 30일 10:44:35
+2026년 08월 31일 07:01:00
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,13 @@
-#260330
-def solution(numbers, target):    
-    def dfs(total, index):
+#260831
+def solution(numbers, target):
+    def dfs(index, current_sum):
         if index == len(numbers):
-            if total == target:
+            if current_sum == target:
                 return 1
             else:
                 return 0
                 
-        return dfs(total + numbers[index], index + 1) + dfs(total - numbers[index], index + 1)
-        
+        return dfs(index + 1, current_sum + numbers[index]) + \
+               dfs(index + 1, current_sum - numbers[index])
+    
     return dfs(0, 0)
