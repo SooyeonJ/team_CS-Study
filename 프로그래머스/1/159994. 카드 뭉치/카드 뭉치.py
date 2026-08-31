bb9cd845da0fc6959e1185b3829c474c39418cf4
dori-2i <asj7020@gmail.com>
2026-08-31 11:39:15 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,13 @@
+def solution(cards1, cards2, goal):
+    
+    for word in goal:
+        if cards1 and cards1[0] == word:
+            cards1.remove(word)
+        elif cards2 and cards2[0] == word:
+            cards2.remove(word)
+        else:
+            return 'No'
+    return 'Yes'
+    
+    
+            
