7d610d3ddfa5326d936ee6568d76d124ce2e0670
dori-2i <asj7020@gmail.com>
2026-09-16 19:11:39 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,4 +1,10 @@
 def solution(array, commands):
+    '효율적인 알고리즘 : 리스트 컴프리핸션 사용'
+    return [sorted(array[i-1:j])[k-1] for i,j,k in commands]
+    
+    
+    
+    '''
     result=[]
     result2=[]
     for x in commands:
@@ -6,7 +12,7 @@ def solution(array, commands):
             result=sorted(array[x[0]-1:x[1]])
         result2.append(result[x[2]-1])
     return result2
-            
+    '''        
             
     
     
