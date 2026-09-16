a1d9c8db3ce0a8e1a6d8744d0a3b5e19297922a0
dori-2i <asj7020@gmail.com>
2026-09-16 19:04:46 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,13 @@
 def solution(array, commands):
-    
     result=[]
-    for i,j,k in commands:
-        result.append((sorted(array[i-1:j])[k-1]))
-        
-    return result
+    result2=[]
+    for x in commands:
+        for y in x:
+            result=sorted(array[x[0]-1:x[1]])
+        result2.append(result[x[2]-1])
+    return result2
             
+            
+    
+    
     
