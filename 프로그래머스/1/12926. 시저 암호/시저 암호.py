be6a2322011d0df3d0b5d824507392f5a07e7132
dori-2i <asj7020@gmail.com>
2026-09-16 18:28:55 +0900

    [level 1] Title: 시저 암호, Time: 0.65 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,19 +1,27 @@
 def solution(s, n):
     result=[]
-    
     for x in s:
-        if x.isupper():
-            ch=chr((ord(x)-ord('A')+n)%26+ord('A'))
-            result.append(ch)
-        elif x.islower():
-            ch=chr((ord(x)-ord('a')+n)%26+ord('a'))
-            result.append(ch)
-        else:
+        if x==' ':
             result.append(x)
-            
-        
+        elif x.islower():
+            if ord(x)+n>ord('z'):
+                result.append(chr(ord(x)-26+n))
+            else:
+                result.append(chr(ord(x)+n))
+        elif x.isupper():
+            if ord(x)+n>ord('Z'):
+                result.append(chr(ord(x)-26+n))
+            else:
+                result.append(chr(ord(x)+n))
+    
+    print(ord('z'),ord('Z'),ord('a'),ord('e'))
     return ''.join(result)
+
+
+    
             
+    
+    
     
     
         
