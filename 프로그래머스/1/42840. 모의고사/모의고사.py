3234ec1904a1a58306671f960f0ec4e5a5608981
dori-2i <asj7020@gmail.com>
2026-09-18 13:28:56 +0900

    [level 1] Title: 모의고사, Time: 1.82 ms, Memory: 13.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,19 @@
 def solution(answers):
-    giveUp1=[1,2,3,4,5]*len(answers)
-    giveUp2=[2, 1, 2, 3, 2, 4, 2, 5]*len(answers)
-    giveUp3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]*len(answers)
-    
-    count=[0,0,0]
-    for x1,x2,x3,y in zip(giveUp1,giveUp2,giveUp3,answers):
-        if x1==y:
-            count[0]+=1
-        if x2==y:
-            count[1]+=1
-        if x3==y:
-            count[2]+=1
-    
-    return [i+1 for i,result in enumerate(count) if result==max(count)]
-        
-        
-        
-        
+    gum1=[1,2,3,4,5]*len(answers)
+    gum2=[2,1,2,3,2,4,2,5]*len(answers)
+    gum3=[3,3,1,1,2,2,4,4,5,5]*len(answers)
+    result =[0,0,0]
+    for i in range(len(answers)):
+        if answers[i]==gum1[i]:
+            result[0]+=1
+        if answers[i]==gum2[i]:
+            result[1]+=1
+        if answers[i]==gum3[i]:
+            result[2]+=1
+    l=max(result)
+    conclusion=[]
+    for i in range(len(result)):
+        if result[i]==l:
+              conclusion.append(i+1)
+                   
+    return conclusion
