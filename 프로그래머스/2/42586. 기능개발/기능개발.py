b3c15b4dde55c39880357b788816ba0b7737c6e4
dori-2i <asj7020@gmail.com>
2026-09-18 18:09:40 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,31 @@
 def solution(progresses, speeds):
+    '''
+    남은 작업량 계산하는 방법
+    - 남은 작업량 : 100 - progress
+    - 하루 작업 속도 : speed
+    - 필요한 일수 : 남은 작업량 / 하루 작업 속도
+        (만약에 % !=0 이면 올림 처리한다.)
+    '''
+    stack = [1]
+    remain = 100 - progresses[0]
+    day = remain//speeds[0]  if remain%speeds[0] == 0 else (remain//speeds[0] + 1)
+    for i in range(1,len(progresses)):
+        if (100 - progresses[i])/speeds[i]<=day:
+            stack[-1]+=1
+        else:
+            stack.append(1)
+            remain=100 - progresses[i]
+            day = remain//speeds[i]  if remain%speeds[i] == 0 else (remain//speeds[i] + 1)
+            
+    return stack
+            
     
-    last_work = [(100-x+y-1)//y for x,y in zip(progresses,speeds)]
-    result=[]
     
-    
-    while last_work:
-        current = last_work.pop(0)
-        count = 1
         
-        while last_work and last_work[0]<=current:
-            last_work.pop(0)
-            count+=1
     
-        result.append(count)
+        
+            
     
-    return result
             
             
     
